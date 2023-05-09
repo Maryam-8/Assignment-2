a7c6478fc52c63dbb35541c2826743225d2a9331
Abdullah <76426103+Maryam-8@users.noreply.github.com>
2023-05-09 22:36:41 +0500

    Upload

---
+++ 19101001-031 Web API's assignment/AssignmentWebApi/Controllers/ContactApiController.cs
@@ -0,0 +1,43 @@
+﻿using AssignmentWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssignmentWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactApiController : ControllerBase
+    {
+        private readonly ContactContext _dbContext;
+        public ContactApiController(ContactContext dbContaxt)
+        {
+            _dbContext = dbContaxt;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContact()
+        {
+            return await _dbContext.contact.ToListAsync();
+        }
+
+
+        
+
+
+        [HttpPut]
+        public async Task<ActionResult<Contact>> putContact(int id, Contact contact)
+        {
+            _dbContext.Entry(contact).State = EntityState.Modified;
+
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        
+
+
+        
+    }
+}
